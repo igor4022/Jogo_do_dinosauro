0bea16f01a66bd9039bdcce76ecee7d7eb63002a
igor4022 <tic80padraolua@gmail.com>
2025-07-24 20:36:03 -0300

    finnalisação do jogo

--- arquivo.js
+++ arquivo.js
@@ -7,4 +7,23 @@ let jump = () => {
         setTimeout(() => {esqueleto.classList.remove('jump')}, 1000)
 }
 
+let loop = setInterval(() => {
+
+    let ralseiMovimento = ralsei.offsetLeft
+    let esqueletoMovimento = +window.getComputedStyle(esqueleto).bottom.replace('px', '')
+
+    console.log(esqueletoMovimento)
+
+    if (ralseiMovimento <= 50 && ralseiMovimento > 0 && esqueletoMovimento < 100) {
+
+        ralsei.style.animation = 'none'
+        ralsei.style.left = `${50}px`
+
+        esqueleto.src = 'imagens/dee7a38c4c7a3b9b0653ce54cb0f2084.gif'
+        esqueleto.style.width = '500px'
+        ralsei.src = 'imagens/dee7a38c4c7a3b9b0653ce54cb0f2084.gif'
+        ralsei.style.width = '0px'
+    } 
+}, 5)
+
 document.addEventListener('keydown', jump)
